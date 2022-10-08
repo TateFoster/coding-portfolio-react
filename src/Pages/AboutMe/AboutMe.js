d2a3923c60ef34133124d03c89f9d793a3bfdfee
Dragon Lore <tate.j.foster@gmail.com>
2022-10-08 13:47:05 -0600

    added icons for tech in and changed size and spacing for them

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -16,10 +16,52 @@ export default function AboutMe() {
 		<div className="container">
 			<p>Hi there!</p>
 			<p>
-				I'm a full stack web designer with a passion for creating fun and easy
+				I'm a full stack web developer with a passion for creating fun and easy
 				to use applications!
 			</p>
 			<h3>Technologies:</h3>
+			<div className="allIcons">
+				<div className="iconBox">
+					<DiJavascript1 className="icons" />
+					JavaScript
+				</div>
+				<div className="iconBox">
+					<DiHtml5 className="icons" />
+					HTML5
+				</div>
+				<div className="iconBox">
+					<DiCss3 className="icons" />
+					CSS3
+				</div>
+				<div className="iconBox">
+					<DiNodejsSmall className="icons" />
+					NodeJS
+				</div>
+				<div className="iconBox">
+					<SiExpress className="icons" />
+					Express.js
+				</div>
+				<div className="iconBox">
+					<SiHandlebarsdotjs className="icons" />
+					Handlebars
+				</div>
+				<div className="iconBox">
+					<DiReact className="icons" />
+					React
+				</div>
+				<div className="iconBox">
+					<DiBootstrap className="icons" />
+					Bootstrap
+				</div>
+				<div className="iconBox">
+					<DiMysql className="icons" />
+					MySQL
+				</div>
+				<div className="iconBox">
+					<DiMongodb className="icons" />
+					MongoDB
+				</div>
+			</div>
 		</div>
 	);
 }
