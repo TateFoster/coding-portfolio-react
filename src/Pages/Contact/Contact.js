441426b5d67accf09045e9117fd6b873313e8c26
Dragon Lore <tate.j.foster@gmail.com>
2022-10-09 10:47:14 -0600

    setting up box in contact for making a mailer input brought in resume for download

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,8 +3,25 @@ import "./Contact.css";
 export default function Contact() {
 	return (
 		<div className="containerContact">
-			<p>Phone: (720)496-3767</p>
-			<p>Email: tate.j.foster@gmail.com</p>
+			<div className="emailBox">
+				<p className="selfInfo">
+					Phone: (720)496-3767 <br></br>Email: tate.j.foster@gmail.com
+				</p>
+				<label for="nameInput">Name:</label>
+				<input type="text" id="nameInput" placeholder="Your name here!"></input>
+				<label for="emailInput">Email:</label>
+				<input
+					type="text"
+					id="emailInput"
+					placeholder="Your email here!"
+				></input>
+				<label for="messageInput">Message:</label>
+				<input
+					type="text"
+					id="messageInput"
+					placeholder="What would you like to tell me?"
+				></input>
+			</div>
 		</div>
 	);
 }
